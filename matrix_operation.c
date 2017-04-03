181b009daa6ba4e2986de3c0d0f4373ff7e15426
shine700 <sudipto.shine@gmail.com>
2017-04-03 15:03:24 +0600

    Add files via upload

---
+++ matrix_operation.c
@@ -0,0 +1,24 @@
+/******
+    MATRIX OPRATION
+    name : shine
+    date : 3-4-17
+    *******/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int choice;
+    printf("\n\t\t\t\t \\\\======== MATRIX OPERATION'S ========// \n");
+    printf("\n\t 1. Summetion ");
+    printf("\t 2. Subtraction ");
+    printf("\t 2. Division ");
+    printf("\t 4. Multiplication ");
+    printf("\t 5. Inverse \n\n");
+
+    printf("\n Enter your choice :  \n");
+    scanf("%d",&choice);
+
+    /** this program is under construction **/
+}
